e9048a58819c996fe23577c71bab73c7a268e0d3
Alexis <alexismichellemee@gmail.com>
2023-03-22 19:07:52 +0100

    Amélioration visuelle banière sur mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,7 +56,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Bienvenue sur mon Portfolio</span>
-                <h1>{`Bonjour! Je m'appelle Alexis :`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Dev", "Front-End", "Fullstack" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Bonjour! Je m'appelle Alexis :`}<br/> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Dev", "Front-End", "Fullstack" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Jeune développeur passionné et curieux, je m'intéresse aux programmes et aux codes depuis ma plus tendre enfance. Après une formation effectuée chez OpenClassrooms, j'espère à présent trouver un job dans le développement web et parfaire mes compétences.</p>
                   <button onClick={() => console.log('connect')}>Me Contacter <ArrowRightCircle size={25} /></button>
               </div>}
